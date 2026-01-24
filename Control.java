596e69ba44e8e0f7949f272926097d897f591e8c
Vadim <gabderahmanov99@gmail.com>
2026-01-24 19:05:32 +0300

    click

--- Control.java
+++ Control.java
@@ -3,12 +3,12 @@ package org.vlg;
 import javax.swing.*;
 import java.awt.*;
 
-public class Control extends JPanel {
-    public Control() {
+public class Controls extends JPanel {
+    public Controls(Game game) {
         setLayout(new FlowLayout());
-        add(new Button("ok"));
-        add(new Button("add"));
-
+        add(new Button("1x", new ImageIcon("assets/supper.jpg"), game::addObject));
+        add(new Button("2x", new ImageIcon("assets/supper.jpg"), () -> {
+        }));
     }
-
 }
+
